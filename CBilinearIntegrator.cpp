9cb9bfcb10cbcbf5af97d6972a1435b7db1df844
stocktonx <stocktonx@me.com>
2015-12-07 15:59:23 -0500

    Create CBilinearIntegrator.cpp

---
+++ CBilinearIntegrator.cpp
@@ -0,0 +1,42 @@
+#pragma once
+
+class CBilinearIntegrator
+{
+protected:
+	double m_dGain;
+	double m_dZ;
+public:
+	CBilinearIntegrator()				// constructor
+	{
+		m_dGain = 0.0;
+		m_dZ = 0.0;
+	}
+	inline void init(double g)			// initialize
+	{
+		m_dGain = g;
+		m_dZ = 0;
+	}
+	inline void reset()
+	{
+		m_dZ = 0;
+	}
+	inline void setGain(double g)			// set Gain
+	{
+		m_dGain = g;
+	}
+	inline double getZ()					// get Delay
+	{
+		return m_dZ;
+	}
+	inline double getGain()					// get Gain
+	{
+		return m_dGain;
+	}
+	inline double doIntegrate(double &input)// Integrate
+	{
+		double v = input*m_dGain;			// process node v
+		double output = v + m_dZ;			// process output
+		m_dZ = v + output;					// process z
+		return output;
+	}
+};
